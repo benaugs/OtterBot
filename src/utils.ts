ed727af26e80bd4fcebb849ec42d3693ed9dd812
benaugs <benaugs@gmail.com>
2025-04-01 21:39:22 +0500

    Update utils.ts

--- src/utils.ts
+++ src/utils.ts
@@ -1,6 +1,6 @@
 export const minutesToMilliseconds = (minutes: number): number => minutes * 60000
 
-export const formatTime = (ms: number): string => {
+export const formatTime = (ms: number): integer => {
   const totalSeconds = Math.floor(ms / 1000);
   const minutes = Math.floor(totalSeconds / 60);
   const seconds = totalSeconds % 60;
